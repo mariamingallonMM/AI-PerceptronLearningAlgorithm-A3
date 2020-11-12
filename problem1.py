34e6cba11936daf0e3756ac09b10471a83fb1831
mariamingallonMM <maria.mingallon@mottmac.com>
2020-11-13 10:31:43 +1300

    first commit

---
+++ problem1.py
@@ -0,0 +1,10 @@
+"""
+This should generate an output file called output1.csv. With each iteration of your PLA (each time we go through all examples), your program must print a new line to the output file, containing a comma-separated list of the weights w_1, w_2, and b in that order. Upon convergence, your program will stop, and the final values of w_1, w_2, and b will be printed to the output file (see example). This defines the decision boundary that your PLA has computed for the given dataset.
+
+Note: When implementing your PLA, in case of tie (sum of w_jx_ij = 0), please follow the lecture note and classify the datapoint as -1.
+
+What To Submit. problem1.py, which should behave as specified above. Before you submit, the RUN button on Vocareum should help you determine whether or not your program executes correctly on the platform.
+
+"""
+
+
